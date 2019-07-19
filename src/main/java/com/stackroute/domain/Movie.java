c37afd8a7c07376fbc2412311d267549173f651d
Anshuman3301 <51943125+Anshuman3301@users.noreply.github.com>
2019-07-19 10:46:56 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -8,30 +8,32 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
+    //inistantiate an actor object.
     private Actor actor;
 
-    /*public Movie(Actor actor) {
-        this.actor = actor;
-    }*/
-
+    //set the actor
     public void setActor(Actor actor) {
         this.actor = actor;
     }
 
+    //print the movie actor.
     public void printAboutMovieActor()
     {
         actor.printActor();
     }
 
+    //print the bean factory after setting its values.
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println(beanFactory);
     }
 
+    //set the name of the bean and print it.
     public void setBeanName(String s) {
         System.out.println(s);
 
     }
 
+    //set the application context and print it.
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println(applicationContext);
     }
