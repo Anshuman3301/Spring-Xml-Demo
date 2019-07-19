baa9aa0bee01bc021fd7e6911e88e72bdc3d6a4c
Anshuman3301 <51943125+Anshuman3301@users.noreply.github.com>
2019-07-19 10:40:59 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -1,17 +1,20 @@
 package com.stackroute.domain;
 
 public class Actor {
+    //give the attributes of acor.
     private String name;
     private String gender;
     private int age;
 
 
+    //initialise an actor using constructor.
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
         this.age = age;
     }
 
+    //print the actor along with its attributes.
     public void printActor()
     {
         System.out.println("Actor's Name:"+name+"\n"+"Gender:"+gender+"\n"+"Age:"+age);
