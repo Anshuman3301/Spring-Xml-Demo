9f429545c12c25ac77b9e4190547f25111e61441
Anshuman3301 <51943125+Anshuman3301@users.noreply.github.com>
2019-07-19 10:31:41 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,17 @@
 package com.stackroute.domain;
 
 public class Movie {
+    
+    //create an actor object
     private Actor actor;
 
+    //initialise the actor in movie class
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
 
+    //print about the actor.
     public void printAboutMovieActor()
     {
         actor.printActor();
